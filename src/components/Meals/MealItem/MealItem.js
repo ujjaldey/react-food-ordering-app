86a27748e4b1478f73de9e91083632f32f307bba
Ujjal Dey <ujjaldey@gmail.com>
2022-04-14 00:54:11 +0800

    #145

--- src/components/Meals/MealItem/MealItem.js
+++ src/components/Meals/MealItem/MealItem.js
@@ -1,9 +1,23 @@
+import { useContext } from 'react';
 import React from 'react';
 import classes from './MealItem.module.css';
 import MealItemForm from './MealItemForm';
+import CartContext from '../../../store/cart-context';
 
 const MealItem = props => {
+  const cartCtx = useContext(CartContext);
+
   const price = `$${props.price.toFixed(2)}`;
+
+  const addToCartHandler = amount => {
+    cartCtx.addItem({
+      id: props.id,
+      name: props.name,
+      amount: amount,
+      price: props.price
+    });
+  };
+
   return <li className={classes.meal}>
     <div>
       <h3>{props.name}</h3>
@@ -11,7 +25,7 @@ const MealItem = props => {
       <div className={classes.price}>{price}</div>
     </div>
     <div>
-      <MealItemForm id={props.id}></MealItemForm>
+      <MealItemForm onAddToCart={addToCartHandler}></MealItemForm>
     </div>
   </li>;
 };
--- src/components/UI/Input.js
+++ src/components/UI/Input.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import classes from './Input.module.css';
 
-const Input = props => {
+// React.forwardRef() is required so that we can use useRef in the custom compoenent Input in MealItemForm component
+// this ref is the forwarded ref which is set in MealItemForm component
+const Input = React.forwardRef((props, ref) => {
   return <div className={classes.input}>
     <label htmlFor={props.input.id}>{props.label}</label>
     {/* ... = ensures all the key value pairs in the input object we receive on props.input are added as props to <input/> */}
     {/* say if the input object contains {type: 'text'}, then this will make sure the input element will have type="text" attribute */}
-    <input {...props.input} />
+    <input ref={ref} {...props.input} />
   </div>;
-};
+});
 
 export default Input;
