6f654fe98fe147c60657594c527cff09b5e6ebcd
Ujjal Dey <ujjaldey@gmail.com>
2022-04-14 01:20:26 +0800

    #146

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,12 +1,27 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import classes from './Cart.module.css'
 import Modal from '../UI/Modal';
+import CartContext from '../../store/cart-context';
+import CartItem from './CartItem';
 
 const Cart = props => {
+  const cartCtx = useContext(CartContext);
+
+  const totalAmount = `${cartCtx.totalAmount.toFixed(2)}`;
+  const hasItems = cartCtx.items.length > 0;
+
+  const cartItemRemoveHandler = id => { };
+  const cartItemAddHandler = item => { };
+
   const cartItems = (
     <ul className={classes['cart-items']}>{ // classes['cart-items'] because the class name has '. otherwise we can just use classes.name
-      [{ id: 'c1', name: 'Sushi', amount: 2, price: 12.99 }]
-        .map(item => <li>{item.name}</li>)
+      cartCtx.items.map(item => <CartItem
+        key={item.id}
+        name={item.name}
+        amount={item.amount}
+        price={item.price}
+        onRemove={cartItemRemoveHandler.bind(null, item.id)} // .bind preconfigure the function execution, and pre configure the parameters when it will get executed. we are not passing the parameters directly, as this is just a reference, not actually calling the function.
+        onAdd={cartItemAddHandler.bind(null, item)} />)
     }</ul>
   );
 
@@ -15,11 +30,11 @@ const Cart = props => {
       {cartItems}
       <div className={classes.total}>
         <span>Total Amount</span>
-        <span>35.62</span>
+        <span>{totalAmount}</span>
       </div>
       <div className={classes.actions}>
         <button className={classes['button-alt']} onClick={props.onCloseForCart}>Close</button>
-        <button className={classes.button}>Order</button>
+        {hasItems && <button className={classes.button}>Order</button>}
       </div>
     </Modal>
   );
