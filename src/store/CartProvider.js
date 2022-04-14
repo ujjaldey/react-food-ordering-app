2f0d4f8056d3a2492bf2241e253fd4ad7c179c8e
Ujjal Dey <ujjaldey@gmail.com>
2022-04-14 09:08:52 +0800

    #147

--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -8,8 +8,22 @@ const defaultCartState = {
 
 const cartReducer = (state, action) => {
   if (action.type === 'ADD') {
-    const updatedItems = state.items.concat(action.item); // unlike push, it does not add to the array, rather return a new array
     const updatedTotalAmount = state.totalAmount + action.item.price * action.item.amount;
+    const existingCartItemIndex = state.items.findIndex(item => item.id === action.item.id);
+    const existingCartItem = state.items[existingCartItemIndex];
+    let updatedItems;
+
+    if (existingCartItem) {
+      const updatedItem = {
+        ...existingCartItem,
+        amount: existingCartItem.amount + action.item.amount
+      };
+      updatedItems = [...state.items];
+      updatedItems[existingCartItemIndex] = updatedItem;
+    } else {
+      updatedItems = state.items.concat(action.item);  // unlike push, it does not add to the array, rather return a new array
+    }
+
     return {
       items: updatedItems,
       totalAmount: updatedTotalAmount
