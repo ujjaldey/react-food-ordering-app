d19ce60cba245a2296854a73870fe387b0bbc3e6
Ujjal Dey <ujjaldey@gmail.com>
2022-04-16 20:25:33 +0800

    #149

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,17 +1,38 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import CartIcon from '../Cart/CartIcon';
 import classes from './HeaderCartButton.module.css'
 import CartContext from '../../store/cart-context'
 
 const HeaderCartButton = props => {
+  const [btnIsHighlighted, setBtnIsHighlighted] = useState(false);
   const cartCtx = useContext(CartContext);
+  const { items } = cartCtx;
 
-  const numberOfCartItems = cartCtx.items.reduce((currNumber, item) => {
+  const numberOfCartItems = items.reduce((currNumber, item) => {
     return currNumber + item.amount;
   }, 0);
 
+  const btnClasses = `${classes.button} ${btnIsHighlighted ? classes.bump : ''}`;
+
+  useEffect(() => {
+    if (cartCtx.items.length === 0) {
+      return;
+    }
+
+    setBtnIsHighlighted(true); // as we are using useEffect, the whole component will be rendered when we change the state, i.e. btnIsHighlighted
+
+    const timer = setTimeout(() => {
+      setBtnIsHighlighted(false);
+    }, 300); // change the state to false to remove the bump class after 300ms (same duration as the animation)
+
+    // cleanup function
+    return () => {
+      clearTimeout(timer);
+    }
+  }, [items]); // adding items as the dependency. so whenever items changes, this will be called
+
   return (
-    <button className={classes.button} onClick={props.onClickHeaderCartButton}>
+    <button className={btnClasses} onClick={props.onClickHeaderCartButton}>
       <span className={classes.icon}>
         <CartIcon />
       </span>
